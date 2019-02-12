76e5aff830033db4b7278ee9417ca83fbdcebfd1
Siva Manivannan <siva@redoxengine.com>
2019-02-11 21:34:08 -0600

    basic setup working

---
+++ src/web/components/welcome/welcome.tsx
@@ -0,0 +1,39 @@
+import * as React from "react";
+
+export const Welcome = () => {
+  return (
+    <>
+    <h1><b>REDOX</b> Takehome Assessment</h1>
+    <p>
+      Not to be mistaken with pseudo-classes, pseudo-elements allow you to style certain parts of a document. We can style after or before an element, the first letter or first line, the selection and the backdrop. Originally in CSS1 and CSS2 the pseudo -elements
+      started with a colon (:) which was the same as pseudo-classes. To separate the two in CSS3 pseudo elements now start with a double colon (::) this is not supported in all versions of Internet Explorer, and you can still use one colon.
+      </p>
+        <blockquote>
+          <b>It is alive!</b> Awesome. You got the starter kit working.
+        </blockquote>
+
+        <h2>Why is it called a Pseudo-Element?</h2>
+        <p> The word pseudo is a Greek word that stands for “lying, false” and an element in HTML could be a paragraph or a header. Put the two together and we create an alternative description of a false paragraph or a false header and this is what the pseudo-element
+          is, a fake element which alters your HTML after load.
+        </p>
+        <p>
+      Not to be mistaken with pseudo-classes, pseudo-elements allow you to style certain parts of a document. We can style after or before an element, the first letter or first line, the selection and the backdrop. Originally in CSS1 and CSS2 the pseudo -elements
+      started with a colon (:) which was the same as pseudo-classes. To separate the two in CSS3 pseudo elements now start with a double colon (::) this is not supported in all versions of Internet Explorer, and you can still use one colon.
+      </p>
+        <blockquote>
+          Donec ullamcorper nulla non metus auctor fringilla. Donec id elit non mi porta gravida at eget metus. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus
+          mus. Aenean lacinia bibendum nulla sed consectetur.
+        </blockquote>
+
+        <h2>Why is it called a Pseudo-Element?</h2>
+        <p> The word pseudo is a Greek word that stands for “lying, false” and an element in HTML could be a paragraph or a header. Put the two together and we create an alternative description of a false paragraph or a false header and this is what the pseudo-element
+          is, a fake element which alters your HTML after load.
+        </p>
+        <details>
+          <summary>Copyright 2015.</summary>
+          <p>More information about the copyright and further details can be hidden</p>
+        </details>
+    </>
+  );
+};
+
