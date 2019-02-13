df9d411819583c9b9b8983590a1f07e53fd5ad17
Siva Manivannan <sivabalan.manivannan@gmail.com>
2019-02-12 20:45:40 -0600

    prune

--- src/web/index.tsx
+++ src/web/index.tsx
@@ -1,27 +1,35 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import "./main.css";
-import { Welcome } from './components/welcome/welcome.tsx'
+import { Welcome } from "./components/welcome/welcome.tsx";
 
 const App = () => {
   return (
     <>
-<article>
-  <aside>
-    <nav>
-      <h3>Sidebar Navigation</h3>
-      <ul>
-        <li><a href="#">Welcome</a></li>
-        <li><a href="#">Link 2</a></li>
-        <li><a href="#">Link 3</a></li>
-        <li><a href="#">Link 4</a></li>
-      </ul>
-    </nav>
-  </aside>
-  <main>
-    <Welcome/>
-  </main>
-</article>      
+      <article>
+        <aside>
+          <nav>
+            <h3>Navigation</h3>
+            <ul>
+              <li>
+                <a href="#">Welcome</a>
+              </li>
+              <li>
+                <a href="#">Link 2</a>
+              </li>
+              <li>
+                <a href="#">Link 3</a>
+              </li>
+              <li>
+                <a href="#">Link 4</a>
+              </li>
+            </ul>
+          </nav>
+        </aside>
+        <main>
+          <Welcome />
+        </main>
+      </article>
     </>
   );
 };
