d0dc872cefb71252d80e1454969e4991ad9b574f
Siva Manivannan <siva@redoxengine.com>
2019-02-13 23:37:32 -0600

    migration updated

---
+++ src/server/knexfile.ts
@@ -0,0 +1,14 @@
+// Update with your config settings.
+
+module.exports = {
+  client: 'postgresql',
+  connection: 'postgresql://redox:redox@localhost:6432/redoxdb',
+  pool: {
+    min: 2,
+    max: 10
+  },
+  migrations: {
+    tableName: 'knex_migrations',
+    extension: 'ts'
+  }
+};
