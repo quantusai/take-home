056fb1f546870258ea683a5cf86739e95f9acc26
Siva Manivannan <siva@redoxengine.com>
2019-03-04 10:55:37 -0600

    sqlite added

--- README.md
+++ README.md
@@ -1,10 +1,6 @@
 # Take-Home - SPA
 
 ## Getting things up and running
-- Install Docker on your computer
-  - [MacOS](https://hub.docker.com/editions/community/docker-ce-desktop-mac)
-  - [Windows](https://hub.docker.com/editions/community/docker-ce-desktop-windows)
-  - [Ubuntu Linux](https://docs.docker.com/install/linux/docker-ce/ubuntu/)
 - Install Node v10
     - [Node](https://nodejs.org/en/download/) 
 - Clone this repoistory
--- src/server/knexfile.ts
+++ src/server/knexfile.ts
@@ -1,14 +1,10 @@
 // Update with your config settings.
-
-const env = process.env.NODE_ENV || '';
-const address = env === 'development' ? 'postgres:5432' : 'localhost:6432'
 module.exports = {
-  client: 'pg',
-  connection: `postgresql://redox:redox@${address}/redoxdb`,
-  pool: {
-    min: 2,
-    max: 10
+  client: 'sqlite3',
+  connection: {
+    filename: './db.sqlite',
   },
+  useNullAsDefault: true,
   migrations: {
     tableName: 'knex_migrations',
     extension: ['js', 'ts']
